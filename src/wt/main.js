50052f263607fc21b51c4f056fd8274ef3b6d02b
Sylphur <neosylphur@gmail.com>
2024-10-08 01:20:40 +0300

    feat: add main wt logic

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import path from 'path';
+import os from 'os';
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpus = os.cpus().length;
+    const workerFile = path.join(path.resolve(), 'src', 'wt', 'worker.js');
+    const workers = [];
+
+    for (let i = 0; i < cpus; i++) {
+        workers.push(
+        new Promise((resolve, reject) => {
+            const worker = new Worker(workerFile, {
+            workerData: i + 10,
+            });
+            worker.on("message", resolve);
+            worker.on("error", reject);
+        })
+        );
+    }
+
+    const results = await Promise.allSettled(workers);
+
+    const logs = results.map((result) => ({
+        status: result.status === `fulfilled` ? "resolved" : "error",
+        data: result.value ? result.value : null,
+    }));
+    console.log(logs);
 };
 
 await performCalculations();
