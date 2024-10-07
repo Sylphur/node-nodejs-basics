54d9fea77e27f2d1352e74fa51896f03d734d16e
Sylphur <neosylphur@gmail.com>
2024-10-07 20:03:36 +0300

    feat: add delete.js solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fsp from 'fs/promises';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const pathToFile = path.join(path.resolve(), 'src', 'fs', 'files', 'fileToRemove.txt');
+    try {
+        await fsp.rm(pathToFile);
+        console.log('fileToRemove.txt has been deleted');
+        
+    } catch (error) {
+        console.error('FS operation failed');
+    }
 };
 
 await remove();
