e6f90dc9443763f103b55596dea2aed8bf1b9662
Sylphur <neosylphur@gmail.com>
2024-10-07 20:56:04 +0300

    feat: add env.js solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    let result = '';
+
+    const env = process.env;
+    const keys = Object.keys(env).filter((key) => key.startsWith(prefix));
+    keys.map((key) => {
+        result += `${key}=${process.env[key]}; `;
+    })
+    console.log(result.slice(0, -2));
+    
 };
 
 parseEnv();
